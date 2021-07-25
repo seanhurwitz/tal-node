5ce278cde57c1fd1e73e8ea3390b60c668d6d329
seanhurwitz <57170877+seanhurwitz@users.noreply.github.com>
2021-07-25 07:13:44 +0200

    Commit

---
+++ code20210711.js
@@ -0,0 +1,79 @@
+/*
+
+Javascript
+
+--LOOSELY / DYNAMICALLY TYPED--
+
+DATATYPES
+---
+string
+number - both integers AND floats/doubles
+boolean
+nully values
+---
+
+*/
+
+// STRINGS
+// console.log("HELLO" + " WORLD");
+// console.log("HELLO dude");
+// console.log("This is a string!");
+// console.log("this is a string!");
+// console.log(typeof "hello"); //string
+
+// NUMBERS
+// console.log(23 + 3);
+// console.log("23" + 3); //turn the 3 into a string and concatenate = 233
+// console.log(5 * 4.122);
+// console.log(typeof 8); // number
+// console.log(typeof 8.2344); // number
+// console.log(65 / 0);
+// console.log(2 / 0.000000000005);
+// console.log(3 / undefined);
+
+//BOOLEANS
+// console.log(true);
+// console.log(false);
+// console.log(typeof true); //boolean
+
+//Falsey values: in a conditional, the will result in a boolean 'false'
+// console.log(""); //"" is a 'falsey' value
+// console.log(0);
+// console.log(null);
+// console.log(undefined);
+
+//Truthy Values
+// console.log("truthyy");
+
+// if (0) {
+//   console.log("CONDITIONAL");
+// } //THIS WILL NOT RUN
+
+// if ("") {
+//   console.log("CONDITIONAL");
+// } //THIS WILL NOT RUN
+
+// if (null) {
+//   console.log("CONDITIONAL");
+// } //THIS WILL NOT RUN
+
+// if (undefined) {
+//   console.log("CONDITIONAL");
+// } //THIS WILL NOT RUN
+
+// if (1) {
+//   console.log("CONDITIONAL");
+// } //THIS WILL NOT RUN
+
+// if (" ") {
+//   console.log("CONDITIONAL");
+// } //THIS WILL NOT RUN
+
+//NULLY VALUES
+// console.log(null);
+// console.log(typeof null);
+
+// console.log(undefined);
+// console.log(typeof undefined);
+//These don't have an essential difference as they
+// will always result in falsey values
