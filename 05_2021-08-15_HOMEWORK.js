eed66d99ed9e656f69f965875b7f9deeabde4efe
seanhurwitz <57170877+seanhurwitz@users.noreply.github.com>
2021-08-15 08:02:06 +0200

    Update 05_2021-08-15_HOMEWORK.js

---
+++ 05_2021-08-15_HOMEWORK.js
@@ -0,0 +1,24 @@
+// This will test your skills of varibles and conditionals
+/* 
+SPEC
+-- I want to go for a RUN! But I want to know if I CAN!
+-- What would prevent me from running?
+--- I usually go in the morning, so if it's later than 10AM I won't go.
+---- BUT: if it's winter I WILL go after 10AM, because it doesn't get so hot in the afternoon.
+--- I won't go if it's raining.
+--- I won't go if I'm feeling sick.
+
+Challenge: build an IF statement that will determine if I will go for a run based on variables:
+-isItMorning
+-isItWinter
+-isItRaining
+-amIHealthy
+
+Try different values for each variable and try console log each combination.
+BONUS: use javascript's new Date() to get the current hours for isItMorning.
+
+Use if/Else or a singular IF statement using && and ||
+
+Hatzlocha!
+
+*/
